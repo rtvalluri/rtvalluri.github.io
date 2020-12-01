87cabd9171b8afe39994497345c98df3fc1fa6c6
rtvalluri <valluriraviteja@gmail.com>
2020-12-01 13:28:58 +0530

    scripts deleted

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const startYear = 2014;
-   const currentYear = new Date().getFullYear() - startYear;
-   const months = new Date().getMonth();
-   const exp = currentYear + ' years ' + months + ' months';
-   document.querySelector('#experience').innerText = exp;
-})
